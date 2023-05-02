1b235f58a55369b14dcc17e1ad3d838276137f76
Ominsi <kienenm14@gmail.com>
2023-05-02 15:27:22 -0400

    Moved Files To src folder

--- BookSorter.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-public class BookSorter implements Comparator<Book>{
-
-	@Override
-	public int compare(Book o1, Book o2){
-		return o1.getTitle().compareToIgnoreCase(o2.getTitle());
-	}
-}
